212881b66fad256e0aafa39d13230b07daf54fd9
ekkusa <dayesofficial@gmail.com>
2024-09-11 23:53:42 +0800

    r65: Add VIAL support

---
+++ keyboards/rk/r65/ansi/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4E, 0x4A, 0xB7, 0x6C, 0x2B, 0x62, 0xF9, 0x71}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
