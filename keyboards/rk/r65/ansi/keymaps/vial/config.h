e2a95cac64280685489244a83c70b14bc7f93c42
ekkusa <dayesofficial@gmail.com>
2024-09-11 23:55:21 +0800

    r65/vial: Disable some features
    * Disable keyoverride, combo and oneshot modifiers to save space.

--- keyboards/rk/r65/ansi/keymaps/vial/config.h
+++ keyboards/rk/r65/ansi/keymaps/vial/config.h
@@ -5,3 +5,6 @@
 #define VIAL_KEYBOARD_UID {0x4E, 0x4A, 0xB7, 0x6C, 0x2B, 0x62, 0xF9, 0x71}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+/* Saving Space */
+#define NO_ACTION_ONESHOT
